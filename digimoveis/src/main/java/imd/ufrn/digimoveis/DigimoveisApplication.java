8ad370fdb31a5b57dfa80cc3e1c22fd4850a02b1
igor bastos <igor.bastos@live.com>
2025-06-25 15:44:14 -0300

    Add files via upload

---
+++ digimoveis/src/main/java/imd/ufrn/digimoveis/DigimoveisApplication.java
@@ -0,0 +1,13 @@
+package imd.ufrn.digimoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigimoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigimoveisApplication.class, args);
+	}
+
+}
---
+++ digimoveis/src/test/java/imd/ufrn/digimoveis/DigimoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package imd.ufrn.digimoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigimoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
